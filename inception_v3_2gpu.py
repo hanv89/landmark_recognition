a2bab8ae5087b642fc5c8b3cd2075448b8ae7cd2
hanv2 <hanv2@vng.com.vn>
2019-01-12 15:46:59 +0700

    fix

--- inception_v3_2gpu.py
+++ inception_v3_2gpu.py
@@ -96,15 +96,15 @@ for i, layer in enumerate(base_model.layers):
 
 # we chose to train the top 2 inception blocks, i.e. we will freeze
 # the first 249 layers and unfreeze the rest:
-for layer in model.layers[:249]:
+for layer in parallel_model.layers[:249]:
    layer.trainable = False
-for layer in model.layers[249:]:
+for layer in parallel_model.layers[249:]:
    layer.trainable = True
 
 # we need to recompile the model for these modifications to take effect
 # we use SGD with a low learning rate
 from keras.optimizers import SGD
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
+parallel_model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # we train our model again (this time fine-tuning the top 2 inception blocks
 # alongside the top Dense layers
