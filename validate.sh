bc6d88ee35dd6b2a573954632b2b8c9ef3a97166
hanv2 <hanv2@vng.com.vn>
2019-02-25 14:18:22 +0700

    update validate

---
+++ validate.sh
@@ -0,0 +1,2 @@
+python3 validate.py --valdir=data/Public --input=data/publicTest.csv --model=$1/finetune/model.h5 --class_index=$1/label.index --size=$2
+python3 validate.py --valdir=data/finalPrivateTest/private_test_3_9/ --input=data/finalPrivateTest.csv --model=$1/finetune/model.h5 --class_index=$1/label.index --size=$2
