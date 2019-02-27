d49bba206921b5e81c7ade3ea2fda9626b505151
hanv2 <hanv2@vng.com.vn>
2019-02-27 10:03:24 +0700

    continue to train xception

--- train.sh
+++ train.sh
@@ -1,3 +1,5 @@
-python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=densenet_169 --train_epochs=20 --train_steps_per_epoch=2000 --finetune_epochs=100 --finetune_steps_per_epoch=2000 --freeze=139
-python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=xception --train_epochs=20 --train_steps_per_epoch=2000 --finetune_epochs=100 --finetune_steps_per_epoch=2000 --freeze=56
-python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --train_epochs=20 --train_steps_per_epoch=2000 --finetune_epochs=100 --finetune_steps_per_epoch=2000 --freeze=73
+# python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=densenet_169 --train_epochs=20 --train_steps_per_epoch=2000 --finetune_epochs=200 --finetune_steps_per_epoch=2000 --freeze=139
+# python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=xception --train_epochs=20 --train_steps_per_epoch=2000 --finetune_epochs=200 --finetune_steps_per_epoch=2000 --freeze=56
+# 
+python3 finetune.py --data=./data/TrainVal/ --mode=finetune --net=xception --finetune_epochs=500 --finetune_steps_per_epoch=2000 --freeze=56 --load_model=output/xception-20190224-090007/finetune/model.h5
+python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --train_epochs=20 --train_steps_per_epoch=2000 --finetune_epochs=200 --finetune_steps_per_epoch=2000 --freeze=73
