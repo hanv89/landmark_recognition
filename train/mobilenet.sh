0e3637ddec00ee70eb2a45560f89351fc89e461c
hanv2 <hanv2@vng.com.vn>
2019-03-11 11:18:20 +0700

    update train params

--- train/mobilenet.sh
+++ train/mobilenet.sh
@@ -1,5 +1,5 @@
 python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 \
                     --train_epochs=10 --train_steps_per_epoch=1200 \
                     --finetune_epochs=120 --finetune_steps_per_epoch=1200 \
-                    --freeze=55 --dropout=0 \
-                    --workers=2 --batch=64 --crop=0
+                    --freeze=55 --dropout=0.5 \
+                    --workers=2 --batch=64 --crop=0.2 --width=0 --height=0
