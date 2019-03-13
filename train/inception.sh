5a948d5311ba5b96c72089d99114f335d27e0458
hanv2 <hanv2@vng.com.vn>
2019-03-13 16:43:00 +0700

    fix

--- train/inception.sh
+++ train/inception.sh
@@ -8,10 +8,10 @@ python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mob
                     --train_epochs=10 --train_steps_per_epoch=1200 \
                     --finetune_epochs=120 --finetune_steps_per_epoch=1200 \
                     --freeze=91 --dropout=0.5 \
-                    --workers=2 --batch=64 --crop=0 --width=0.2 --height=0.2 --finetune_lr=0.0002
+                    --workers=2 --batch=64 --crop=0 --width=0.2 --height=0.2 --finetune_lr=0.0004
 
 python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 \
                     --train_epochs=10 --train_steps_per_epoch=1200 \
                     --finetune_epochs=120 --finetune_steps_per_epoch=1200 \
                     --freeze=117 --dropout=0.5 \
-                    --workers=2 --batch=64 --crop=0 --width=0.2 --height=0.2 --finetune_lr=0.0004
+                    --workers=2 --batch=64 --crop=0 --width=0.2 --height=0.2 --finetune_lr=0.0002
