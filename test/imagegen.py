2ad020a343dc77d33c90d245d2c3819cf87e5c2f
hanv2 <hanv2@vng.com.vn>
2019-03-15 11:19:34 +0700

    add img gen demo

---
+++ test/imagegen.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+from tensorflow import keras
+
+print(tf.VERSION)
+print(tf.keras.__version__)
+
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras import backend as K
+
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import json
+import argparse
+import time
+import math
+import sys
+import os
+
+# tf.enable_eager_execution() #only for test run
+
+parser = argparse.ArgumentParser(description='Landmark Detection Training then Finetune')
+
+#Directories
+parser.add_argument('--data', default='./data', type = str, help = 'Data dir')
+#Augmentation parameters
+parser.add_argument('--horizontal_flip', type=bool, default=True)
+parser.add_argument('--zoom', type=float, default=0)
+parser.add_argument('--shear', type=float, default=0)
+parser.add_argument('--width', type=float, default=0)
+parser.add_argument('--height', type=float, default=0)
+parser.add_argument('--rotate', type=int, default=0)
+parser.add_argument('--channel', type=float, default=0)
+
+args = parser.parse_args()
+
+
+train_datagen = image.ImageDataGenerator(
+  rescale=1./255,
+  shear_range=args.shear,
+  zoom_range=args.zoom,
+  width_shift_range=args.width,
+  height_shift_range=args.height,
+  rotation_range=args.rotate,
+  horizontal_flip=args.horizontal_flip,
+  channel_shift_range=args.channel)
+
+validation_generator = train_datagen.flow_from_directory(
+  directory=args.data,
+  target_size=(299, 299),
+  color_mode='rgb',
+  batch_size=32,
+  class_mode='sparse',
+  shuffle=True,
+  seed=42,
+)
+while True:
+    images, labels = next(validation_generator)
+    plt.figure(figsize=(100,200))
+    for i in range (0,32):
+        plt.subplot(4,8,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(images[i])
+        plt.xlabel(labels[i])
+    plt.show()
--- train/bigbatch4.sh
+++ train/bigbatch4.sh
@@ -1,5 +1,5 @@
 python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --workers=2 \
-                    --train_epochs=5 --train_steps_per_epoch=300 \
+                    --train_epochs=4 --train_steps_per_epoch=150 \
                     --finetune_lr1=0.0002 --finetune_epochs1=20 --finetune_steps_per_epoch1=300 \
                     --finetune_lr2=0.0004 --finetune_epochs2=80 --finetune_steps_per_epoch2=300 \
                     --freeze=91 --dropout=0.5 --l2=0 \
