877cea69dc2ac37dc44d0702f0df71b936b84294
hanv2 <hanv2@vng.com.vn>
2019-03-16 16:48:42 +0700

    try

--- train/densenet1.sh
+++ train/densenet1.sh
@@ -1,13 +1,13 @@
 python3 finetune_v2.py --data=./data/TrainValAug/ --mode=train_then_finetune --net=densenet_169 --workers=2 \
                     --train_lr=0.0004 --train_epochs=1 --train_steps_per_epoch=40 \
-                    --finetune_lr1=0.0002 --finetune_epochs1=3 --finetune_steps_per_epoch1=1280 \
-                    --finetune_lr2=0.0002 --finetune_epochs2=1 --finetune_steps_per_epoch2=1280 \
+                    --finetune_lr1=0.0002 --finetune_epochs1=3 --finetune_steps_per_epoch1=80 \
+                    --finetune_lr2=0.0002 --finetune_epochs2=1 --finetune_steps_per_epoch2=80 \
                     --freeze=139 --dropout=0.5 --l2=0.05 \
                     --batch=64
 python3 finetune_v2.py --data=./data/TrainValAug/ --mode=train_then_finetune --net=densenet_169 --workers=2 \
                     --train_lr=0.0004 --train_epochs=1 --train_steps_per_epoch=40 \
-                    --finetune_lr1=0.0002 --finetune_epochs1=3 --finetune_steps_per_epoch1=1280 \
-                    --finetune_lr2=0.0002 --finetune_epochs2=1 --finetune_steps_per_epoch2=1280 \
+                    --finetune_lr1=0.0002 --finetune_epochs1=3 --finetune_steps_per_epoch1=80 \
+                    --finetune_lr2=0.0002 --finetune_epochs2=1 --finetune_steps_per_epoch2=80 \
                     --freeze=51 --dropout=0.5 --l2=0.05 \
                     --batch=32
 python3 finetune_v2.py --data=./data/TrainValAug/ --mode=train_then_finetune --net=densenet_169 --workers=2 \
