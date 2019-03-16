3965444b53c91081938a945a2f694bfacbb90966
hanv2 <hanv2@vng.com.vn>
2019-03-16 15:15:19 +0700

    fix train

--- train.sh
+++ train.sh
@@ -7,7 +7,8 @@
 
 ./train/densenet1.sh
 ./train/densenet2.sh
-./train/bigbatch1.sh
-./train/bigbatch2.sh
-./train/bigbatch3.sh
-./train/bigbatch4.sh
+# ./train/bigbatch1.sh
+# ./train/bigbatch2.sh
+# ./train/bigbatch3.sh
+# ./train/bigbatch4.sh
+./train/bigbatch5.sh
--- train/densenet1.sh
+++ train/densenet1.sh
@@ -1,4 +1,4 @@
-python3 finetune.py --data=./data/TrainValAug/ --mode=train_then_finetune --net=densenet_169 --workers=2 \
+python3 finetune_v2.py --data=./data/TrainValAug/ --mode=train_then_finetune --net=densenet_169 --workers=2 \
                     --train_epochs=4 --train_steps_per_epoch=160 \
                     --finetune_lr1=0.0002 --finetune_epochs1=10 --finetune_steps_per_epoch1=320 \
                     --finetune_lr2=0.0002 --finetune_epochs2=40 --finetune_steps_per_epoch2=320 \
