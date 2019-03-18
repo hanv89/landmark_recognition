ee04f4a5f173714a8a9d01870c8662ba306ab27f
hanv2 <hanv2@vng.com.vn>
2019-03-18 17:04:28 +0700

    move back densenet

--- train/bigbatch5.sh
+++
@@ -1,6 +0,0 @@
-python3 finetune_v2.py --data=./data/TrainValAug/ --mode=train_then_finetune --net=densenet_169 --workers=2 \
-                    --train_lr=0.0004 --train_epochs=4 --train_steps_per_epoch=640 \
-                    --finetune_lr1=0.0002 --finetune_epochs1=10 --finetune_steps_per_epoch1=1280 \
-                    --finetune_lr2=0.0002 --finetune_epochs2=40 --finetune_steps_per_epoch2=1280 \
-                    --freeze=367 --dropout=0.5 --l2=0.05 \
-                    --batch=64
--- train/densenet2.sh
+++ train/densenet2.sh
@@ -4,3 +4,9 @@ python3 finetune_v2.py --data=./data/TrainValAug/ --mode=train_then_finetune --n
                     --finetune_lr2=0.0002 --finetune_epochs2=40 --finetune_steps_per_epoch2=2560 \
                     --freeze=51 --dropout=0.5 --l2=0.05 \
                     --batch=32
+python3 finetune_v2.py --data=./data/TrainValAug/ --mode=train_then_finetune --net=densenet_169 --workers=2 \
+                    --train_lr=0.0004 --train_epochs=4 --train_steps_per_epoch=640 \
+                    --finetune_lr1=0.0002 --finetune_epochs1=10 --finetune_steps_per_epoch1=1280 \
+                    --finetune_lr2=0.0002 --finetune_epochs2=40 --finetune_steps_per_epoch2=1280 \
+                    --freeze=367 --dropout=0.5 --l2=0.05 \
+                    --batch=64
