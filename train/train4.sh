a1fdee0f0b8224b14dc3b3225610d7df1e8f0b8b
hanv2 <hanv2@vng.com.vn>
2019-03-20 17:00:42 +0700

    change train mobilenet

--- train/train4.sh
+++ train/train4.sh
@@ -1,3 +1,10 @@
+
+python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=2 \
+                    --train_lr=0.001 --train_epochs=10 --train_steps_per_epoch=400 \
+                    --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=400 \
+                    --finetune_lr2=0.00001 --finetune_epochs2=90 --finetune_steps_per_epoch2=400 \
+                    --freeze=0 --dropout1=0.5 --l21=0.01  \
+                    --batch=64
 python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=2 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=400 \
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=400 \
@@ -15,10 +22,4 @@ python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=400 \
                     --finetune_lr2=0.0001 --finetune_epochs2=60 --finetune_steps_per_epoch2=400 \
                     --freeze=91 \
-                    --batch=64
-python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=2 \
-                    --train_lr=0.001 --train_epochs=10 --train_steps_per_epoch=400 \
-                    --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=400 \
-                    --finetune_lr2=0.00001 --finetune_epochs2=90 --finetune_steps_per_epoch2=400 \
-                    --freeze=0 --dropout1=0.5 --l21=0  \
                     --batch=64
