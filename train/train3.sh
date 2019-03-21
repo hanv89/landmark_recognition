f4b4b6186e5d85ce185fda42780f79871cb79ad1
hanv2 <hanv2@vng.com.vn>
2019-03-21 10:21:13 +0700

    up

--- train/train3.sh
+++ train/train3.sh
@@ -40,13 +40,13 @@ python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=400 \
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=400 \
                     --finetune_lr2=0.0001 --finetune_epochs2=80 --finetune_steps_per_epoch2=400 \
-                    --freeze=0 \
+                    --freeze=295 \
                     --batch=64
 python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=1 --workers=2 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=400 \
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=400 \
                     --finetune_lr2=0.0001 --finetune_epochs2=80 --finetune_steps_per_epoch2=400 \
-                    --freeze=295 \
+                    --freeze=0 \
                     --batch=64
 python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=1 --workers=2 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=400 \
