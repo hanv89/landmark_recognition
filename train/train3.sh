8ceb235808575c78d3a9963e0f23efad1e93eb60
hanv2 <hanv2@vng.com.vn>
2019-03-22 16:20:34 +0700

    training for results

--- train/train3.sh
+++ train/train3.sh
@@ -14,17 +14,11 @@ python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=400 \
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=400 \
                     --finetune_lr2=0.0002 --finetune_epochs2=120 --finetune_steps_per_epoch2=400 \
-                    --freeze=295 --dropout1=0.5 --l21=0.1 \
+                    --freeze=295 --dropout1=0 --l21=0.1 \
                     --batch=64
 python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=2 --workers=2 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=400 \
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=400 \
                     --finetune_lr2=0.0002 --finetune_epochs2=120 --finetune_steps_per_epoch2=400 \
-                    --freeze=295 --dropout1=0.5 --l21=0.01 \
-                    --batch=64
-python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=2 --workers=2 \
-                    --train_lr=0.0001 --train_epochs=30 --train_steps_per_epoch=400 \
-                    --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=400 \
-                    --finetune_lr2=0.0002 --finetune_epochs2=120 --finetune_steps_per_epoch2=400 \
-                    --freeze=532 --dropout1=0 --l21=0 \
+                    --freeze=295 --dropout1=0 --l21=0.01 \
                     --batch=64
