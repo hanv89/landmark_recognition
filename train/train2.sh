ec830e003936e4ee27718ebc76444e9f37a45554
hanv2 <hanv2@vng.com.vn>
2019-03-22 17:26:21 +0700

    lets be greedy

--- train/train2.sh
+++ train/train2.sh
@@ -9,7 +9,7 @@ python3 finetune_v2.py --data=./data/TrainVal/ --mode=finetune --net=nasnetmobil
                     --load_model=./output/nasnetmobile-20190322-134348/finetune/sgd/model.h5 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=400 \
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=400 \
-                    --finetune_lr2=0.00005 --finetune_epochs2=60 --finetune_steps_per_epoch2=400 \
+                    --finetune_lr2=0.00001 --finetune_epochs2=60 --finetune_steps_per_epoch2=400 --finetune_lr_decay=0.1 \
                     --freeze=295 \
                     --batch=64
 python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=2 --workers=2 \
