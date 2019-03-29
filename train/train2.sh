0293baa69ca07db8bdbba193a25dafb9ca95c937
hanv2 <hanv2@vng.com.vn>
2019-03-29 10:31:24 +0700

    fix train

--- train/train2.sh
+++ train/train2.sh
@@ -4,16 +4,22 @@ python3 finetune_v2.py --data=./data/TrainValExt/ --mode=train_then_finetune --n
                     --finetune_lr2=0.001 --finetune_epochs2=200 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1 \
                     --freeze=295 --dropout1=0 --l21=0.01 \
                     --batch=32
-python3 finetune_v2.py --data=./data/TrainVal/ --mode=finetune --net=nasnetmobile --dense_layers=1 --workers=2 \
-                    --load_model=./output/nasnetmobile-20190328-091055/finetune/sgd/model.h5 \
+python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=1 --workers=2 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=200 \
-                    --finetune_lr1=0.0001 --finetune_epochs1=20 --finetune_steps_per_epoch1=800 \
-                    --finetune_lr2=0.001 --finetune_epochs2=80 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1 \
-                    --freeze=295 --dropout1=0 --l21=0.01 \
+                    --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
+                    --finetune_lr2=0.001 --finetune_epochs2=200 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1\
+                    --freeze=295 --dropout1=0 --l21=0.05 \
                     --batch=32
 python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=1 --workers=2 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
                     --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
                     --finetune_lr2=0.001 --finetune_epochs2=80 --finetune_steps_per_epoch2=800 \
                     --freeze=295 --dropout1=0 --l21=0  --bn1=False \
+                    --batch=32
+python3 finetune_v2.py --data=./data/TrainVal/ --mode=finetune --net=nasnetmobile --dense_layers=1 --workers=2 \
+                    --load_model=./output/nasnetmobile-20190328-091055/finetune/sgd/model.h5 \
+                    --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=200 \
+                    --finetune_lr1=0.0001 --finetune_epochs1=20 --finetune_steps_per_epoch1=800 \
+                    --finetune_lr2=0.001 --finetune_epochs2=80 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1 \
+                    --freeze=295 --dropout1=0 --l21=0.01 \
                     --batch=32
