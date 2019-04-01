48c68ec1eb6c0b38fd5af3dde0fe220f3d7a3e6c
hanv2 <hanv2@vng.com.vn>
2019-04-01 14:18:44 +0700

    give adam a try

--- train/train5.sh
+++ train/train5.sh
@@ -1,4 +1,9 @@
-
+python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=1 --workers=2 \
+                    --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
+                    --finetune_lr1=0.00005 --finetune_epochs1=80 --finetune_steps_per_epoch1=800 --finetune_lr_decay=0.1 \
+                    --finetune_lr2=0.001 --finetune_epochs2=0 --finetune_steps_per_epoch2=800 \
+                    --freeze=295 --dropout1=0 --l21=0 \
+                    --batch=32
 python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=inception_v3 --dense_layers=2 --workers=2 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
                     --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
