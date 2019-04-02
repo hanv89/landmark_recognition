7ef7ac37e205849d024d1685f6997e65bc8c4527
hanv2 <hanv2@vng.com.vn>
2019-04-02 08:37:28 +0700

    up train

--- train/train4.sh
+++ train/train4.sh
@@ -1,16 +1,16 @@
-python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=1 --workers=2 \
-                    --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
-                    --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
-                    --finetune_lr2=0.001 --finetune_epochs2=80 --finetune_steps_per_epoch2=800 \
-                    --freeze=295 --dropout1=0 --l21=0 \
-                    --batch=32 \
-                    --horizontal_flip=False --zoom_in=0 --zoom_out=0 --shear=0 --width=0 --height=0 --rotate=0 --channel=0
-python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=1 --workers=2 \
-                    --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=400 \
-                    --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
-                    --finetune_lr2=0.001 --finetune_epochs2=200 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1\
-                    --freeze=295 --dropout0=0 --l21=0.1 \
-                    --batch=32
+# python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=1 --workers=2 \
+#                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
+#                     --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
+#                     --finetune_lr2=0.001 --finetune_epochs2=80 --finetune_steps_per_epoch2=800 \
+#                     --freeze=295 --dropout1=0 --l21=0 \
+#                     --batch=32 \
+#                     --horizontal_flip=False --zoom_in=0 --zoom_out=0 --shear=0 --width=0 --height=0 --rotate=0 --channel=0
+# python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=1 --workers=2 \
+#                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=400 \
+#                     --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
+#                     --finetune_lr2=0.001 --finetune_epochs2=200 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1\
+#                     --freeze=295 --dropout0=0 --l21=0.1 \
+#                     --batch=32
 python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=1 --workers=2 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=400 \
                     --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
