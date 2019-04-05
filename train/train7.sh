2e3ed0fe0bac1d3399c33599fdc4471834072873
hanv2 <hanv2@vng.com.vn>
2019-04-05 12:23:04 +0700

    up train

--- train/train7.sh
+++ train/train7.sh
@@ -1,21 +1,3 @@
-python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=inception_v3 --dense_layers=2 --workers=2 \
-                    --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
-                    --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
-                    --finetune_lr2=0.001 --finetune_epochs2=150 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1\
-                    --freeze=311 --dropout1=0 --l21=0 \
-                    --batch=32
-python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=xception --dense_layers=2 --workers=2 \
-                    --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
-                    --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
-                    --finetune_lr2=0.001 --finetune_epochs2=150 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1\
-                    --freeze=132 --dropout1=0 --l21=0 \
-                    --batch=32
-python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=resnet_50 --dense_layers=2 --workers=2 \
-                    --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
-                    --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
-                    --finetune_lr2=0.001 --finetune_epochs2=150 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1\
-                    --freeze=175 --dropout1=0 --l21=0 \
-                    --batch=32
 python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=2 --workers=2 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
                     --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
@@ -39,4 +21,22 @@ python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=
                     --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
                     --finetune_lr2=0.001 --finetune_epochs2=150 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1\
                     --freeze=707 --dropout1=0 --l21=0 \
+                    --batch=32
+python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=inception_v3 --dense_layers=2 --workers=2 \
+                    --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
+                    --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
+                    --finetune_lr2=0.001 --finetune_epochs2=150 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1\
+                    --freeze=311 --dropout1=0 --l21=0 \
+                    --batch=32
+python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=xception --dense_layers=2 --workers=2 \
+                    --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
+                    --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
+                    --finetune_lr2=0.001 --finetune_epochs2=150 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1\
+                    --freeze=132 --dropout1=0 --l21=0 \
+                    --batch=32
+python3 finetune_v2.py --data=./data/TrainVal/ --mode=train_then_finetune --net=resnet_50 --dense_layers=2 --workers=2 \
+                    --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
+                    --finetune_lr1=0.001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
+                    --finetune_lr2=0.001 --finetune_epochs2=150 --finetune_steps_per_epoch2=800 --finetune_lr_decay=0.1\
+                    --freeze=175 --dropout1=0 --l21=0 \
                     --batch=32
