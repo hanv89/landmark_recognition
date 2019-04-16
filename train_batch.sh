4b6d17d29fa27aeabd7e098a45eeba2ad27d77ff
hanv2 <hanv2@vng.com.vn>
2019-04-16 16:57:06 +0700

    fix train

--- train_batch.sh
+++ train_batch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
-./train/train1.sh
+# ./train/train1.sh
 ./train/train2.sh
-./train/train3.sh
-./train/train4.sh
-./train/train5.sh
-./train/train6.sh
-./train/train7.sh
-./train/train8.sh
+# ./train/train3.sh
+# ./train/train4.sh
+# ./train/train5.sh
+# ./train/train6.sh
+# ./train/train7.sh
+# ./train/train8.sh
