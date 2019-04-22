bafa3da9c1fb775fdf49bce606652e951c86251a
hanv2 <hanv2@vng.com.vn>
2019-04-22 09:23:37 +0700

    up

--- validate/validate.sh
+++ validate/validate.sh
@@ -1,3 +1,3 @@
 echo 'Validating '$1
-python3 validate/validate.py --valdir=data/Public --input=data/publicTest.csv --model=$1 --class_index=output/label.index.csv --size=$2
-python3 validate/validate.py --valdir=data/finalPrivateTest/private_test_3_9/ --input=data/finalPrivateTest.csv --model=$1 --class_index=output/label.index.csv --size=$2
+python3 validate/validate.py --valdir=data/Public --input=data/publicTest.csv --model=$1 --class_index=labels/label.index.csv --size=$2
+python3 validate/validate.py --valdir=data/finalPrivateTest/private_test_3_9/ --input=data/finalPrivateTest.csv --model=$1 --class_index=labels/label.index.csv --size=$2
