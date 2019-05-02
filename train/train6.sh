e07b3911287b12be895f248ac2ead661e905fa13
hanv2 <hanv2@vng.com.vn>
2019-05-02 14:15:33 +0700

    up

--- train/train6.sh
+++ train/train6.sh
@@ -4,7 +4,7 @@
 #                     --finetune_lr2=0.001 --finetune_epochs2=200 --finetune_steps_per_epoch2=800 \
 #                     --freeze=28 --dense1=1280 \
 #                     --dropout0=0.5
-python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=1 --workers=4 \
+python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=nasnetmobile --dense_layers=1 --workers=8 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
                     --finetune_lr2=0.001 --finetune_epochs2=200 --finetune_steps_per_epoch2=800 \
