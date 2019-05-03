4c0a6e593620bb1f7d7ba2be26c27728d9773e16
hanv2 <hanv2@vng.com.vn>
2019-05-03 09:20:39 +0700

    up

--- train/train7.sh
+++ train/train7.sh
@@ -1,28 +1,28 @@
-python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=2 \
+python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=4 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
                     --finetune_lr2=0.001 --finetune_epochs2=200 --finetune_steps_per_epoch2=800 \
                     --freeze=55 --dense1=1280 \
                     --dropout0=0.5 --validation_split=0.1 --seed=1 --use_class_weight=True
-python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=2 \
+python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=4 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
                     --finetune_lr2=0.001 --finetune_epochs2=200 --finetune_steps_per_epoch2=800 \
                     --freeze=55 --dense1=1280 \
                     --dropout0=0.5 --validation_split=0.1 --seed=2 --use_class_weight=True
-python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=2 \
+python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=4 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
                     --finetune_lr2=0.001 --finetune_epochs2=200 --finetune_steps_per_epoch2=800 \
                     --freeze=55 --dense1=1280 \
                     --dropout0=0.5 --validation_split=0.1 --seed=3 --use_class_weight=True
-python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=2 \
+python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=4 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
                     --finetune_lr2=0.001 --finetune_epochs2=200 --finetune_steps_per_epoch2=800 \
                     --freeze=55 --dense1=1280 \
                     --dropout0=0.5 --validation_split=0.1 --seed=4 --use_class_weight=True
-python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=2 \
+python3 finetune.py --data=./data/TrainVal/ --mode=train_then_finetune --net=mobilenet_v2 --dense_layers=1 --workers=4 \
                     --train_lr=0.001 --train_epochs=6 --train_steps_per_epoch=800 \
                     --finetune_lr1=0.0001 --finetune_epochs1=0 --finetune_steps_per_epoch1=800 \
                     --finetune_lr2=0.001 --finetune_epochs2=200 --finetune_steps_per_epoch2=800 \
--- train_batch.sh
+++ train_batch.sh
@@ -4,6 +4,6 @@
 # ./train/train3.sh
 # ./train/train4.sh
 # ./train/train5.sh
-./train/train6.sh
-# ./train/train7.sh
-# ./train/train8.sh
+# ./train/train6.sh
+./train/train7.sh
+./train/train8.sh
